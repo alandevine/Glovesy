99826bf21e9ca2866d2a261fed242d0d9e511867
devina24 <alan.devine24@mail.dcu.ie>
2021-01-14 14:58:31 +0000

    Added test for addEdge

--- src/GCS/src/test/java/viewer/utilTest.java
+++ src/GCS/src/test/java/viewer/utilTest.java
@@ -1,17 +1,31 @@
 package viewer;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Sphere;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class utilTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void setColor() {
     }
 
     // addEdge Tests
-    @org.junit.jupiter.api.Test
+    @Test
     void testStartEndPositions() {
+        Sphere a = new Sphere();
+        a.setLayoutX(0.0);
+        a.setLayoutY(0.0);
 
+        Sphere b = new Sphere();
+        b.setLayoutX(20.0);
+        b.setLayoutY(20.0);
+
+        Line edge = util.addEdge(a, b);
+        Assertions.assertEquals(edge.getStartX(), a.getLayoutX());
+        Assertions.assertEquals(edge.getStartY(), a.getLayoutY());
+        Assertions.assertEquals(edge.getEndX(), b.getLayoutX());
+        Assertions.assertEquals(edge.getEndY(), b.getLayoutY());
     }
 }
