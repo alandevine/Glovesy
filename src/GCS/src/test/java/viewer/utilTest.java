873614141ab1553427a8aa24980b3fb699d8ff74
devina24 <alan.devine24@mail.dcu.ie>
2021-01-14 14:56:55 +0000

    Began unit tests for Util.java

---
+++ src/GCS/src/test/java/viewer/utilTest.java
@@ -0,0 +1,17 @@
+package viewer;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static
+
+class utilTest {
+
+    @org.junit.jupiter.api.Test
+    void setColor() {
+    }
+
+    // addEdge Tests
+    @org.junit.jupiter.api.Test
+    void testStartEndPositions() {
+
+    }
+}
