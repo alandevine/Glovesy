80aeb6fb68be2edc2090fd95cb7b0515ab6ce628
devina24 <alan.devine24@mail.dcu.ie>
2021-01-17 13:54:09 +0000

    Added Camera functionality

--- src/GCS/src/main/java/viewer/View.java
+++ src/GCS/src/main/java/viewer/View.java
@@ -1,23 +1,48 @@
 package viewer;
 
-import javafx.scene.Group;
+import javafx.scene.Camera;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 
 public class View {
-    private final int width;
-    private final int height;
+
+    private final Hand hand;
+    private final Scene scene;
 
     public View (int width, int height) {
-        this.width = width;
-        this.height = height;
+        hand = new Hand(width / 2, height / 2);
+        scene = new Scene(hand, width, height);
+
+        Camera camera = new PerspectiveCamera();
+        scene.setCamera(camera);
+        initKeyboardHandler();
     }
 
     public Scene getScene() {
-        Hand hand = new Hand(width / 2, height / 2);
-        Group handGroup = hand.getHand();
-
-        Scene scene = new Scene(handGroup, width, height);
-
         return scene;
+    }
+
+    /**
+     * Method for adding keyboard events to apply rotation on the overall scene.
+     * The cases below are to be replaced with data received from the glove
+     */
+    private void initKeyboardHandler() {
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            switch (event.getCode()) {
+                case UP:
+                    hand.rotateOnXAxis(+10);
+                    break;
+                case DOWN:
+                    hand.rotateOnXAxis(-10);
+                    break;
+                case LEFT:
+                    hand.rotateOnYAxis(-10);
+                    break;
+                case RIGHT:
+                    hand.rotateOnYAxis(10);
+                    break;
+            }
+        });
     }
 }
