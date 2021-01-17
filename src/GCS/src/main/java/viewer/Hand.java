382c578cc2aebb754e06b023f7e004452c50d2c6
devina24 <alan.devine24@mail.dcu.ie>
2021-01-17 13:51:43 +0000

    slight refactor

--- src/GCS/src/main/java/viewer/Hand.java
+++ src/GCS/src/main/java/viewer/Hand.java
@@ -4,26 +4,32 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Transform;
 
 public class Hand extends Group {
+    Rotate r;
+    Transform t = new Rotate();
 
-    public Hand(int width, int height) {
+    public Hand(int startX, int startY) {
+
+        // Create all nodes in this instance of the hand object at the top left of the screen.
 
         Sphere leftHandBase = new Sphere(10);
-        leftHandBase.setLayoutX(width - 30);
-        leftHandBase.setLayoutY(height + 100);
+        leftHandBase.setLayoutX(-30);
+        leftHandBase.setLayoutY(100);
         util.setColor(leftHandBase, 1.0, 1.0, 0.0);
 
         this.getChildren().add(leftHandBase);
-        this.getChildren().add(new Finger(0.6, width - 60, height + 40));
-        this.getChildren().add(new Finger(0.8, width - 30, height + 10));
-        this.getChildren().add(new Finger(1.0, width, height));
-        this.getChildren().add(new Finger(0.8, width + 30, height + 10));
-        this.getChildren().add(new Finger(0.6, width + 60, height + 20));
+        this.getChildren().add(new Finger(0.6, - 60, 40));
+        this.getChildren().add(new Finger(0.8, - 30, 10));
+        this.getChildren().add(new Finger(1.0, 0, 0));
+        this.getChildren().add(new Finger(0.8, 30, 10));
+        this.getChildren().add(new Finger(0.6, 60, 20));
 
         Sphere rightHandBase = new Sphere(10);
-        rightHandBase.setLayoutX(width + 30);
-        rightHandBase.setLayoutY(height + 100);
+        rightHandBase.setLayoutX(30);
+        rightHandBase.setLayoutY(100);
         util.setColor(rightHandBase, 1.0, 1.0, 0.0);
 
         this.getChildren().add(rightHandBase);
@@ -35,7 +41,7 @@ public class Hand extends Group {
 
         Node curr;
         Node last;
-        // loop through
+        // loop through nodes and create edges
         for (int i = 1; i < size; i++) {
             curr = this.getChildren().get(i) instanceof Finger ? ((Finger) this.getChildren().get(i)).getKnuckle() : this.getChildren().get(i);
             last = this.getChildren().get(i - 1) instanceof Finger ? ((Finger) this.getChildren().get(i - 1)).getKnuckle() : this.getChildren().get(i - 1);
@@ -43,9 +49,23 @@ public class Hand extends Group {
             edge = util.addEdge(curr, last);
             this.getChildren().add(edge);
         }
+
+        // Move this instance of the hand to the provided xy coordinates
+        this.translateXProperty().set(startX);
+        this.translateYProperty().set(startY);
     }
 
-    public Group getHand() {
-        return this;
+    public void rotateOnXAxis(int ang) {
+        r = new Rotate(ang, Rotate.X_AXIS);
+        t = t.createConcatenation(r);
+        this.getTransforms().clear();
+        this.getTransforms().addAll(t);
+    }
+
+    public void rotateOnYAxis(int ang) {
+        r = new Rotate(ang, Rotate.Y_AXIS);
+        t = t.createConcatenation(r);
+        this.getTransforms().clear();
+        this.getTransforms().addAll(t);
     }
 }
