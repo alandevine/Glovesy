c5523d6e1742954c5984bd26e5cb48d26b4b07e6
devina24 <alan.devine24@mail.dcu.ie>
2021-01-27 18:51:55 +0000

    added setColor method for cylinders

--- src/GCS/src/main/java/viewer/util.java
+++ src/GCS/src/main/java/viewer/util.java
@@ -3,6 +3,7 @@ package viewer;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Cylinder;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Sphere;
 
@@ -26,6 +27,13 @@ public class util {
         material.setDiffuseColor(color);
         sphere.setMaterial(material);
     }
+    public static void setColor(Cylinder cylinder, double red, double green, double blue) {
+        PhongMaterial material = new PhongMaterial();
+        Color color = new Color(red, green, blue, 1.0);
+        material.setDiffuseColor(color);
+        cylinder.setMaterial(material);
+    }
+
 
     public static Line addEdge(Node a, Node b) {
         Line line = new Line();
@@ -36,6 +44,4 @@ public class util {
 
         return line;
     }
-
-    public static void rotateGroup(Sphere a) {}
 }
