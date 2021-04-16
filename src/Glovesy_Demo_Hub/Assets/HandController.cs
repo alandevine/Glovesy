c5d5147670870e97e1f2efacebb9f735f7df1843
molons29 <sean.moloney29@mail.dcu.ie>
2021-04-16 15:27:25 +0200

    Model and Rig hand

---
+++ src/Glovesy_Demo_Hub/Assets/HandController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class HandController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    static string InitSerial() {
+        string path = "/dev/ttyACM0";
+
+        StreamReader reader = new 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
