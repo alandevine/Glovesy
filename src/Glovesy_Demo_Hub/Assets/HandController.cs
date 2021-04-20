88b59a7b3d3c0ef55a705ee1f4fe1e52266b3352
molons29 <sean.moloney29@mail.dcu.ie>
2021-04-20 11:34:00 +0200

    Add Reading From Serial

--- src/Glovesy_Demo_Hub/Assets/HandController.cs
+++ src/Glovesy_Demo_Hub/Assets/HandController.cs
@@ -1,24 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Collections;
 using System.IO;
+using System.Threading;
+using UnityEngine;
 
 public class HandController : MonoBehaviour
 {
+    StreamReader reader;
+    int LOADED;
+    string path = "/dev/ttyACM0";
+    string GlovesyData;
+    float[] ParsedData;
+    
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        init_serial(path);
+        InvokeRepeating("ReadSerial",1f,1f);
+    }
+
+    void init_serial(string path) {
+        try {
+            reader = new StreamReader(path);
+            LOADED = 1;
+            return;
+        } catch (FileNotFoundException e) {
+            return;
+        }
         
     }
 
-    static string InitSerial() {
-        string path = "/dev/ttyACM0";
-
-        StreamReader reader = new 
+    void ReadSerial() {
+        Debug.Log(reader.Peek());
+        GlovesyData = reader.ReadLine();
+        return;
     }
+
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
     }
 }
