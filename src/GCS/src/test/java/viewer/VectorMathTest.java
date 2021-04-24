f5c31ba6d0a2c3c30deb9e819f5bd8e6ef7bb2f5
devina24 <alan.devine24@mail.dcu.ie>
2021-04-24 14:54:54 +0100

    added length test

--- src/GCS/src/test/java/viewer/VectorMathTest.java
+++ src/GCS/src/test/java/viewer/VectorMathTest.java
@@ -3,7 +3,10 @@ package viewer;
 import javafx.geometry.Point3D;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static viewer.VectorMath.rotateAboutPoint;
 
 class VectorMathTest {
 
@@ -27,7 +30,17 @@ class VectorMathTest {
     void getAngleXY() {
     }
 
+    /**
+     * Test the distance from the subject to the after rotation.
+     */
     @Test
-    void rotateAboutPoint() {
+    void rotateTestLength() {
+        double angle = 90;
+        Point3D pivot   = new Point3D(0, 0, 0);
+        Point3D subject = new Point3D(1, 1, 1);
+
+        double lengthPreTranslate = pivot.distance(subject);
+        Point3D newPoint = rotateAboutPoint(angle, pivot, subject);
+        assertEquals(lengthPreTranslate, pivot.distance(newPoint));
     }
 }
