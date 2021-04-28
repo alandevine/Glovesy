1ec232031e07529a2b62b66099cc667e0e91b54a
Alan Devine <alanrdevine@gmail.com>
2021-04-28 21:09:29 +0100

    added config label

--- src/GCS/src/main/java/gui/ConfigLabel.java
+++ src/GCS/src/main/java/gui/ConfigLabel.java
@@ -1,2 +1,10 @@
-package gui;public class ConfigLabel {
+package gui;
+
+import javafx.scene.control.Label;
+import org.bson.Document;
+
+public class ConfigLabel extends Label {
+    public ConfigLabel (Document doc) {
+        this.setText(doc.getString("field") + "\t" + doc.get("value"));
+    }
 }
