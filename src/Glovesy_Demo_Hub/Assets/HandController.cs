3b50fa1d1f88eccd90337a6bd84ca6451621e063
molons29 <sean.moloney29@mail.dcu.ie>
2021-04-29 15:22:30 +0200

    Update Calibration

--- src/Glovesy_Demo_Hub/Assets/HandController.cs
+++ src/Glovesy_Demo_Hub/Assets/HandController.cs
@@ -2,19 +2,25 @@
 using System.Collections;
 using System.IO;
 using System.Threading;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class HandController : MonoBehaviour
-{
+public class HandController : MonoBehaviour {
     StreamReader reader;
     string path = "/dev/ttyACM0";
     string GlovesyData;
     float[] ParsedData = {0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f};
-   
+    float[] Min = {-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f};
+    float[] Max = {-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f,-1.0f};
+    float[] NormalizedValues = {0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f};
+    int CALIBRATED = 0;
+    float timer;
+  
     // Start is called before the first frame update
     void Start() {
         init_serial(path);
         InvokeRepeating("ReadSerial",1f,1f);
+        //StartCoroutine(GetMinMax());
     }
 
     void init_serial(string path) {
@@ -25,6 +31,30 @@ public class HandController : MonoBehaviour
             return;
         }
         
+    }
+
+    IEnumerator GetMinMax() {
+        Text instruction = GameObject.Find("Instruction").GetComponent<Text>();
+        Text CountDown = GameObject.Find("Countdown").GetComponent<Text>();
+        if (timer < 5.0f) {
+            instruction.text = "Open your hand as much as possible";
+            for (int i=0; i < 7; i++) {
+                if (Min[i] == -1.0f || ParsedData[i] < Min[i]) {
+                    Min[i] = ParsedData[i];
+                }
+            }
+        } else if (timer < 10.0f) {
+            instruction.text = "Now make a fist as tightly as possible";
+            for (int i=0; i < 7; i++) {
+                if (Max[i] == -1.0f || ParsedData[i] > Max[i]) {
+                    Max[i] = ParsedData[i];
+                }
+            }
+        } else {
+            instruction.text = "";
+            CALIBRATED = 1;
+        }
+        yield return null;
     }
 
     void ReadSerial() {
@@ -39,7 +69,18 @@ public class HandController : MonoBehaviour
         return;
     }
 
+    IEnumerator UpdateTimer() {
+        timer += Time.deltaTime;
+        yield return null;
+    }
+
     // Update is called once per frame
     void Update() {
+
+        if (CALIBRATED == 0) {
+            StartCoroutine(GetMinMax());
+            StartCoroutine(UpdateTimer());
+            Debug.Log(timer);
+        }
     }
 }
