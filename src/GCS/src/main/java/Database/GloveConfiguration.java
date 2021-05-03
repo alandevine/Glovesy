156ed0e419432100ddc04fb1f3cdc04d5efb0045
devina24 <alan.devine24@mail.dcu.ie>
2021-05-03 23:44:25 +0100

    removed unused methods and modified fields

--- src/GCS/src/main/java/Database/GloveConfiguration.java
+++ src/GCS/src/main/java/Database/GloveConfiguration.java
@@ -19,10 +19,8 @@ public class GloveConfiguration {
     private double maxMiddleKnuckleResistance;
     private double minMiddleNeutroResistance;
     private double maxMiddleNeutroResistance;
-    private double minRingKnuckleResistance;
-    private double maxRingKnuckleResistance;
-    private double minRingNeutroResistance;
-    private double maxRingNeutroResistance;
+    private double minRingResistance;
+    private double maxRingResistance;
     private double minPinkyResistance;
     private double maxPinkyResistance;
     private double initialGyroX;
@@ -41,19 +39,13 @@ public class GloveConfiguration {
         this.maxMiddleKnuckleResistance = 0;
         this.minMiddleNeutroResistance = 0;
         this.maxMiddleNeutroResistance = 0;
-        this.minRingKnuckleResistance = 0;
-        this.maxRingKnuckleResistance = 0;
-        this.minRingNeutroResistance = 0;
-        this.maxRingNeutroResistance = 0;
+        this.minRingResistance = 0;
+        this.maxRingResistance = 0;
         this.minPinkyResistance = 0;
         this.maxPinkyResistance = 0;
         this.initialGyroX = 0;
         this.initialGyroY = 0;
         this.initialGyroZ = 0;
-    }
-
-    public double getMinThumbResistance() {
-        return minThumbResistance;
     }
 
     public Document toBson() {
@@ -70,10 +62,8 @@ public class GloveConfiguration {
         config.append("maxMiddleKnuckleResistance", this.maxMiddleKnuckleResistance);
         config.append("minMiddleNeutroResistance",  this.minMiddleNeutroResistance);
         config.append("maxMiddleNeutroResistance",  this.maxMiddleNeutroResistance);
-        config.append("minRingKnuckleResistance",   this.minRingKnuckleResistance);
-        config.append("maxRingKnuckleResistance",   this.maxRingKnuckleResistance);
-        config.append("minRingNeutroResistance",    this.minRingNeutroResistance);
-        config.append("maxRingNeutroResistance",    this.maxRingNeutroResistance);
+        config.append("minRingResistance",          this.minRingResistance);
+        config.append("maxRingResistance",          this.maxRingResistance);
         config.append("minPinkyResistance",         this.minPinkyResistance);
         config.append("maxPinkyResistance",         this.maxPinkyResistance);
         config.append("initialGyroX",               this.initialGyroX);
@@ -96,10 +86,8 @@ public class GloveConfiguration {
         config.put("maxMiddleKnuckleResistance", this.maxMiddleKnuckleResistance);
         config.put("minMiddleNeutroResistance",  this.minMiddleNeutroResistance);
         config.put("maxMiddleNeutroResistance",  this.maxMiddleNeutroResistance);
-        config.put("minRingKnuckleResistance",   this.minRingKnuckleResistance);
-        config.put("maxRingKnuckleResistance",   this.maxRingKnuckleResistance);
-        config.put("minRingNeutroResistance",    this.minRingNeutroResistance);
-        config.put("maxRingNeutroResistance",    this.maxRingNeutroResistance);
+        config.put("minRingResistance",          this.minRingResistance);
+        config.put("maxRingResistance",          this.maxRingResistance);
         config.put("minPinkyResistance",         this.minPinkyResistance);
         config.put("maxPinkyResistance",         this.maxPinkyResistance);
         config.put("initialGyroX",               this.initialGyroX);
@@ -121,10 +109,8 @@ public class GloveConfiguration {
         this.maxMiddleKnuckleResistance = Double.parseDouble(config.getString("maxMiddleKnuckleResistance"));
         this.minMiddleNeutroResistance  = Double.parseDouble(config.getString("minMiddleNeutroResistance"));
         this.maxMiddleNeutroResistance  = Double.parseDouble(config.getString("maxMiddleNeutroResistance"));
-        this.minRingKnuckleResistance   = Double.parseDouble(config.getString("minRingKnuckleResistance"));
-        this.maxRingKnuckleResistance   = Double.parseDouble(config.getString("maxRingKnuckleResistance"));
-        this.minRingNeutroResistance    = Double.parseDouble(config.getString("minRingNeutroResistance"));
-        this.maxRingNeutroResistance    = Double.parseDouble(config.getString("maxRingNeutroResistance"));
+        this.minRingResistance          = Double.parseDouble(config.getString("minRingResistance"));
+        this.maxRingResistance          = Double.parseDouble(config.getString("maxRingResistance"));
         this.minPinkyResistance         = Double.parseDouble(config.getString("minPinkyResistance"));
         this.maxPinkyResistance         = Double.parseDouble(config.getString("maxPinkyResistance"));
         this.initialGyroX               = Double.parseDouble(config.getString("initialGyroX"));
@@ -132,151 +118,23 @@ public class GloveConfiguration {
         this.initialGyroZ               = Double.parseDouble(config.getString("initialGyroZ"));
     }
 
-    public void setMinThumbResistance(double minThumbResistance) {
-        this.minThumbResistance = minThumbResistance;
+    public void setMaxValues(Document minValues) {
+        this.maxThumbResistance         = minValues.getDouble("thumb");
+        this.maxIndexKnuckleResistance  = minValues.getDouble("indexKnuckle");
+        this.maxIndexNeutroResistance   = minValues.getDouble("indexNeutro");
+        this.maxMiddleKnuckleResistance = minValues.getDouble("middleKnuckle");
+        this.maxMiddleNeutroResistance  = minValues.getDouble("middleKnuckle");
+        this.maxRingResistance          = minValues.getDouble("ring");
+        this.maxPinkyResistance         = minValues.getDouble("pinky");
     }
 
-    public double getMaxThumbResistance() {
-        return maxThumbResistance;
-    }
-
-    public void setMaxThumbResistance(double maxThumbResistance) {
-        this.maxThumbResistance = maxThumbResistance;
-    }
-
-    public double getMinIndexKnuckleResistance() {
-        return minIndexKnuckleResistance;
-    }
-
-    public void setMinIndexKnuckleResistance(double minIndexKnuckleResistance) {
-        this.minIndexKnuckleResistance = minIndexKnuckleResistance;
-    }
-
-    public double getMaxIndexKnuckleResistance() {
-        return maxIndexKnuckleResistance;
-    }
-
-    public void setMaxIndexKnuckleResistance(double maxIndexKnuckleResistance) {
-        this.maxIndexKnuckleResistance = maxIndexKnuckleResistance;
-    }
-
-    public double getMinIndexNeutroResistance() {
-        return minIndexNeutroResistance;
-    }
-
-    public void setMinIndexNeutroResistance(double minIndexNeutroResistance) {
-        this.minIndexNeutroResistance = minIndexNeutroResistance;
-    }
-
-    public double getMaxIndexNeutroResistance() {
-        return maxIndexNeutroResistance;
-    }
-
-    public void setMaxIndexNeutroResistance(double maxIndexNeutroResistance) {
-        this.maxIndexNeutroResistance = maxIndexNeutroResistance;
-    }
-
-    public double getMinMiddleKnuckleResistance() {
-        return minMiddleKnuckleResistance;
-    }
-
-    public void setMinMiddleKnuckleResistance(double minMiddleKnuckleResistance) {
-        this.minMiddleKnuckleResistance = minMiddleKnuckleResistance;
-    }
-
-    public double getMaxMiddleKnuckleResistance() {
-        return maxMiddleKnuckleResistance;
-    }
-
-    public void setMaxMiddleKnuckleResistance(double maxMiddleKnuckleResistance) {
-        this.maxMiddleKnuckleResistance = maxMiddleKnuckleResistance;
-    }
-
-    public double getMinMiddleNeutroResistance() {
-        return minMiddleNeutroResistance;
-    }
-
-    public void setMinMiddleNeutroResistance(double minMiddleNeutroResistance) {
-        this.minMiddleNeutroResistance = minMiddleNeutroResistance;
-    }
-
-    public double getMaxMiddleNeutroResistance() {
-        return maxMiddleNeutroResistance;
-    }
-
-    public void setMaxMiddleNeutroResistance(double maxMiddleNeutroResistance) {
-        this.maxMiddleNeutroResistance = maxMiddleNeutroResistance;
-    }
-
-    public double getMinRingKnuckleResistance() {
-        return minRingKnuckleResistance;
-    }
-
-    public void setMinRingKnuckleResistance(double minRingKnuckleResistance) {
-        this.minRingKnuckleResistance = minRingKnuckleResistance;
-    }
-
-    public double getMaxRingKnuckleResistance() {
-        return maxRingKnuckleResistance;
-    }
-
-    public void setMaxRingKnuckleResistance(double maxRingKnuckleResistance) {
-        this.maxRingKnuckleResistance = maxRingKnuckleResistance;
-    }
-
-    public double getMinRingNeutroResistance() {
-        return minRingNeutroResistance;
-    }
-
-    public void setMinRingNeutroResistance(double minRingNeutroResistance) {
-        this.minRingNeutroResistance = minRingNeutroResistance;
-    }
-
-    public double getMaxRingNeutroResistance() {
-        return maxRingNeutroResistance;
-    }
-
-    public void setMaxRingNeutroResistance(double maxRingNeutroResistance) {
-        this.maxRingNeutroResistance = maxRingNeutroResistance;
-    }
-
-    public double getMinPinkyResistance() {
-        return minPinkyResistance;
-    }
-
-    public void setMinPinkyResistance(double minPinkyResistance) {
-        this.minPinkyResistance = minPinkyResistance;
-    }
-
-    public double getMaxPinkyResistance() {
-        return maxPinkyResistance;
-    }
-
-    public void setMaxPinkyResistance(double maxPinkyResistance) {
-        this.maxPinkyResistance = maxPinkyResistance;
-    }
-
-    public double getInitialGyroX() {
-        return initialGyroX;
-    }
-
-    public void setInitialGyroX(double initialGyroX) {
-        this.initialGyroX = initialGyroX;
-    }
-
-    public double getInitialGyroY() {
-        return initialGyroY;
-    }
-
-    public void setInitialGyroY(double initialGyroY) {
-        this.initialGyroY = initialGyroY;
-    }
-
-    public double getInitialGyroZ() {
-        return initialGyroZ;
-    }
-
-    public void setInitialGyroZ(double initialGyroZ) {
-        this.initialGyroZ = initialGyroZ;
+    public void setMinValues(Document minValues) {
+        this.minThumbResistance         = minValues.getDouble("thumb");
+        this.minIndexKnuckleResistance  = minValues.getDouble("indexKnuckle");
+        this.minIndexNeutroResistance   = minValues.getDouble("indexNeutro");
+        this.minMiddleKnuckleResistance = minValues.getDouble("middleKnuckle");
+        this.minMiddleNeutroResistance  = minValues.getDouble("middleKnuckle");
+        this.minRingResistance          = minValues.getDouble("ring");
+        this.minPinkyResistance         = minValues.getDouble("pinky");
     }
 }
