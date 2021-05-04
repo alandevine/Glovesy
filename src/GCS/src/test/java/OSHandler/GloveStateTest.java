718cdbb43ebe1aa2a7f9f3683c9ee474a830e593
devina24 <alan.devine24@mail.dcu.ie>
2021-05-04 20:21:09 +0100

    changed input parameters

--- src/GCS/src/test/java/OSHandler/GloveStateTest.java
+++ src/GCS/src/test/java/OSHandler/GloveStateTest.java
@@ -2,20 +2,20 @@ package OSHandler;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GloveStateTest {
 
     @Test
     void updateStateArrayTooSmall() {
         GloveState gloveState = new GloveState();
-        double[] testData = new double[5];
+        String[] testData = new String[5];
         assertThrows(IllegalArgumentException.class, () -> gloveState.updateState(testData));
     }
     @Test
     void updateStateArrayTooLarge() {
         GloveState gloveState = new GloveState();
-        double[] testData = new double[20];
+        String[] testData = new String[20];
         assertThrows(IllegalArgumentException.class, () -> gloveState.updateState(testData));
     }
 }
